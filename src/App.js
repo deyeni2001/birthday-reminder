0464525985dae7f966835815ee23251cad7d3757
deyeni2001 <deyeni2001@yahoo.com>
2022-01-09 03:18:17 +0100

    Final Commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import React, { useState } from 'react';
-import data from './data';
-import List from './List';
+import React, { useState } from "react";
+import data from "./data";
+import List from "./List";
 function App() {
-  return <h2>reminder project setup</h2>;
+
+  const [birthday, setBirthday] = useState(data)
+  return (
+    <main>
+      <section className="container">
+        <h3>{birthday.length} Birthdays today</h3>
+        <List birthday ={birthday}/>
+        <button onClick={()=> setBirthday([])}>
+          clear all
+        </button>
+      </section>
+    </main>
+  );
 }
 
 export default App;
